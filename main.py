95abf920ed0aa4f76936e501477e277b0c325900
Aarjit <poudelaarjit@gmail.com>
2023-04-01 20:47:44 +0545

    face recognition

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2
+
+cascade_classifier = cv2.CascadeClassifier('haarcascades/haarcascade_profileface.xml')
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, 0)
+    detections = cascade_classifier.detectMultiScale(gray,scaleFactor=1.3,minNeighbors=5)
+    if(len(detections) > 0):
+        (x,y,w,h) = detections[0]
+        frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
